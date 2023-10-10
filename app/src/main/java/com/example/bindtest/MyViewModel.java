9256227d7e929d99993459b05bd03dabaccf06e7
Steven <139094062+XJQ124@users.noreply.github.com>
2023-10-10 03:45:14 -0500

    Update MyViewModel.java

--- app/src/main/java/com/example/bindtest/MyViewModel.java
+++ app/src/main/java/com/example/bindtest/MyViewModel.java
@@ -2,7 +2,7 @@ package com.example.bindtest;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.SharedPreferences;git
+import android.content.SharedPreferences;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
